fe0efc47e5ec837b61f30a76b3335358adc793fd
Muhammet Corduk <om.corduk@gmail.com>
2024-09-01 19:32:49 +0300

    feat: add and draw a player

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,9 @@ use crossterm::event::{Event, KeyCode};
 use crossterm::{event, terminal, ExecutableCommand};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
-use space_invaders::frame::new_frame;
+use space_invaders::frame::{new_frame, Drawable};
 use space_invaders::{frame, render};
-
-enum State {
-    Alive,
-    Dead,
-}
-
-struct Position{
-    x: i32,
-    y: i32,
-}
-
-enum GameObject{
-    Player(Position, State),
-    Enemy(Position, State),
-    Bullet(Position, State),
-}
-
+use space_invaders::player::Player;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -62,14 +46,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     // Game Loop
+    let mut player = Player::new();
+    
     'gameloop: loop {
         // Per-frame limit
-        let curr_frame = new_frame();
-        
+        let mut curr_frame = new_frame();
         // Input
         while event::poll(Duration::default())?{
             if let Event::Key(key_event) = event::read()?{
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
                     KeyCode::Esc => {
                          break 'gameloop;
                     },
@@ -79,6 +66,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
 
         // Draw & render
+        player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
