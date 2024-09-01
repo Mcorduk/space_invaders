30398954c45f94cc4369257d006d160d294405a5
Muhammet Corduk <om.corduk@gmail.com>
2024-09-01 20:59:09 +0300

    feat: add win and lose conditions and finish the game

--- src/player.rs
+++ src/player.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 
 use crate::frame::Drawable;
 use crate::frame::Frame;
+use crate::invaders::Invaders;
 use crate::shot::Shot;
 use crate::NUM_COLS;
 use crate::NUM_ROWS;
@@ -47,6 +48,21 @@ impl Player {
             shot.update(delta);
         }
         self.shots.retain(|shot| !shot.dead()); 
+    }
+
+    pub fn detect_hits(&mut self, invaders: &mut Invaders) -> bool {
+        let mut hit_something = false;
+
+        for shot in self.shots.iter_mut(){
+            if !shot.exploding{
+                if invaders.kill_invader_at(shot.x, shot.y) {
+                    hit_something = true;
+                    shot.explode();
+                }
+            }
+        }
+
+        hit_something
     }
 }
 
